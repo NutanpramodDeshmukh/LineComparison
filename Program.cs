df2ed623c87f63efb284f9f595dc1b1f11a54a3c
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-29 15:19:57 +0530

    Added: Calculate length

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LengthofLine
+namespace LineComparison
 {
-    internal class Program
+    class GFG
     {
-        static void Main(string[] args)
+        
+        static void Length()
         {
+            Console.Write("Enter Lengths");
+
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Length of a Line is");
+            double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)); 
+            
+
+
+        }
+
+        
+        public static void Main()
+        {
+           
+            Length();
         }
     }
 }
