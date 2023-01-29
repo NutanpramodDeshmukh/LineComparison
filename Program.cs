5331ee32f94d777ed3652593a0ea764733f9cca0
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-29 15:44:02 +0530

    Added: new file

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Line");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            
+
+            Console.WriteLine("Enter Second Line");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            double y4 = Convert.ToDouble(Console.ReadLine());
+            double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Lengthis :", Length);
+            
+            double LengthTwo = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Lenght", LengthTwo);
+            if (Length==(LengthTwo))
+            {
+                Console.WriteLine("First and Second Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("First and Second Lines are not Equal");
+            }
+        }
+    }
+    }
+
+
